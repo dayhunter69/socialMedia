c25d79af88ba60519a2390f7c70aaf05f6ed0bc9
anilkunwar <dayhunter69@gmail.com>
2024-04-03 13:02:50 +0545

    configured eslint and added two packages "eslint-plugin-react" and @typescript-eslint/eslint-plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,33 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    '@typescript-eslint/no-var-requires': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      '@typescript-eslint/no-var-requires', 0,
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2020: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked", // Update this line
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/stylistic-type-checked", // Add this line
+    "plugin:react/recommended", // Add this line
+    "plugin:react/jsx-runtime", // Add this line
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"], // Add this line
+    tsconfigRootDir: __dirname, // Add this line
   },
-}
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      {
+        allowConstantExport: true,
+      },
+    ],
+    "@typescript-eslint/no-var-requires": "off", // Keep this existing rule
+  },
+};
